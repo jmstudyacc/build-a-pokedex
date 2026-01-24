1a17982d678ba74da7095bd323a47ea3d6566de0
james <jmstudyacc@gmail.com>
2026-01-24 11:30:05 +0000

    moved intelligence into repl.go

--- main.go
+++ main.go
@@ -1,28 +1,5 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-	"strings"
-)
-
 func main() {
-	// create a scanner to read from os.Stdin
-	scanner := bufio.NewScanner(os.Stdin) // calling this will block & wait for user input and CR
-	for {
-		fmt.Print("Pokedex > ")
-
-		// scanner.Scan() required to start populating a buffer with text
-		scanner.Scan()
-		userInput := scanner.Text() // you then call .Text() on the buffer
-
-		midSlice := strings.ToLower(userInput)
-		// fmt.Printf("DEBUG: %s\n", midSlice)
-
-		inputSlice := strings.Fields(midSlice)
-		// fmt.Printf("DEBUG: %#v (length: %d)\n", inputSlice, len(inputSlice))
-
-		fmt.Printf("Your command was: %v\n", inputSlice[0])
-	}
+	startRepl()
 }
--- repl.go
+++ repl.go
@@ -1,8 +1,32 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strings"
 )
+
+func startRepl() {
+	// create a scanner to read from os.Stdin
+	scanner := bufio.NewScanner(os.Stdin) // calling this will block & wait for user input and CR
+	for {
+		fmt.Print("Pokedex > ")
+
+		// scanner.Scan() required to start populating a buffer with text
+		scanner.Scan()
+
+		// generating a string slice by using cleanInput() on stdin
+		userInput := cleanInput(scanner.Text())
+
+		// checking length of input to avoid panic
+		if len(userInput) == 0 {
+			continue
+		}
+		// print the first position word in the string slice
+		fmt.Printf("Your command was: %v\n", userInput[0])
+	}
+}
 
 func cleanInput(text string) []string {
 	// cleanInput must TRIM leading/trailing whitespaces
