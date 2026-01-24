ee2f82d8147817f26fbaa1e7decbd5573bfb3baf
james <jmstudyacc@gmail.com>
2026-01-24 12:39:51 +0000

    adding command map & callback funcs

--- repl.go
+++ repl.go
@@ -7,6 +7,29 @@ import (
 	"strings"
 )
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error // callback is a function that returns an error
+}
+
+var commandMap map[string]cliCommand
+
+// to avoid circular dependencies, init() used to assign the map before main runs
+func init() {
+	commandMap = map[string]cliCommand{ // map with keys that are strings, and values that are cliCommand structs
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		}, "exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
+}
+
 func startRepl() {
 	// create a scanner to read from os.Stdin
 	scanner := bufio.NewScanner(os.Stdin) // calling this will block & wait for user input and CR
@@ -19,12 +42,23 @@ func startRepl() {
 		// generating a string slice by using cleanInput() on stdin
 		userInput := cleanInput(scanner.Text())
 
-		// checking length of input to avoid panic
+		// checking length of input to avoid panic, just continue if no input received
 		if len(userInput) == 0 {
 			continue
 		}
+
+		command, exist := commandMap[userInput[0]]
+		// if the user input is a command word and exists in the map
+		if exist {
+			// storing the return from command.callback()
+			err := command.callback()
+			// if command.callback() does not return nil an error has occurred
+			if err != nil {
+				fmt.Println("Unknown Command")
+			}
+		}
 		// print the first position word in the string slice
-		fmt.Printf("Your command was: %v\n", userInput[0])
+		// fmt.Printf("Your command was: %v\n", userInput[0])
 	}
 }
 
@@ -38,4 +72,27 @@ func cleanInput(text string) []string {
 			strings.TrimSpace(text),
 		),
 	)
+}
+
+// function to exit out of the pokedex
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	// send a successful exit code back to the function
+	os.Exit(0)
+	return nil
+}
+
+// function to display the available commands - error return value
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:")
+	fmt.Println()
+
+	// range over the commandMap, you need the k&v for the loop
+	for _, command := range commandMap {
+		// access the fields for name and description
+		fmt.Printf("%s: %s\n", command.name, command.description)
+	}
+
+	return nil
 }
